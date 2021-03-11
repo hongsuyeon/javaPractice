fd97f9277a201508a0f1cf447f199f898fb427a4
hongsu <subong3853@naver.com>
2021-03-11 17:02:39 +0900

    재정리..

--- src/Day01_Day02/effectiveJava01/FooInterface.java
+++ src/Day01_Day02/effectiveJava01/FooInterface.java
@@ -1,6 +1,9 @@
 package Day01_Day02.effectiveJava01;
 
 public interface FooInterface {
+
+    static final Foo GOOD_NIGHT = new Foo();
+
     public static Foo getFoo(){
         return new Foo();
     }
